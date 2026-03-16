264edcfd9106452433ff4648c9ee50a8b1098e28
Andrianina <srako079@uOttawa.ca>
2026-03-16 10:22:01 -0400

    Recreated project
    replaced empty project with pre build MVC template project

--- eHotels/obj/Debug/net10.0/eHotels.AssemblyInfo.cs
+++ eHotels/obj/Debug/net10.0/eHotels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eHotels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f23a1f210c1d610c6623dda5f58421431630b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb04b4e1d23ba79323fe78fd86f02aba7f30514")]
 [assembly: System.Reflection.AssemblyProductAttribute("eHotels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eHotels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
