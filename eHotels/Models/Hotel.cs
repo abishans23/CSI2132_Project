ced02e21269153fe1305e4c9edf2c9ec8c407372
abishans23 <abishans23@gmail.com>
2026-03-25 15:24:06 -0400

    hotels

--- eHotels/Models/Hotel.cs
+++ eHotels/Models/Hotel.cs
@@ -6,13 +6,23 @@ class Hotel{
     int chainID {get;set;} //references HotelChain
     string name {get;set;}
     int stars {get;set;}
-    int manager {get;set;} //references Employees
+    string manager {get;set;} //references Employees
     string postalCode {get;set;} //references Address
+    string description {get;set;}
+    string fileName {get;set;}
+    string imageDesc {get;set;}
 
-    public Account(int chainID, string name, string postalCode){
-        this.email = email;
-        this.username = username;
-        this.password = password;
+    //MORE TO COME
+
+    public Hotel(int chainID, string name, int stars, string manager,string postalCode, string descriptio, string fileName, string imageDesc){
+        this.chainID = chainID;
+        this.name = name;
+        this.stars = stars;
+        this.manager = manager;
+        this.postalCode = postalCode;
+        this.description = description;
+        this.fileName = fileName;
+        this.imageDesc = imageDesc
     }
 
 }
--- eHotels/Models/HotelChain.cs
+++ eHotels/Models/HotelChain.cs
@@ -6,10 +6,10 @@ class HotelChain{
     string name {get;set;}
     string postalCode {get;set;} //references Address
 
-    public Account(int chainID, string name, string postalCode){
-        this.email = email;
-        this.username = username;
-        this.password = password;
+    public HotelChain(int chainID, string name, string postalCode){
+        this.chainID = chainID;
+        this.name = name;
+        this.postalCode = postalCode;
     }
 
 }
