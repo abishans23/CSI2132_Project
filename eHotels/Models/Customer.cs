6e0e5fdd4d6273700227f21a1fcd0154f7fd4a9f
abishans23 <abishans23@gmail.com>
2026-03-29 12:13:08 -0400

    cust

--- eHotels/Models/Customer.cs
+++ eHotels/Models/Customer.cs
@@ -1,3 +1,13 @@
 class Customer{
+
+    //primary keys
+    int IDType;
+    int IDNumber;
+
+    //other keys
+    string firstName{get;set;}
+    string lastName{get;set;}
+     string registrationDate{get;set;}
+    
     
 }
