01ff6dc28eb9228ef47a8d2870ef00b53a0988c2
Andrianina <srako079@uOttawa.ca>
2026-04-05 18:59:21 -0400

    Create TableColumnsAndTypes.cs

---
+++ eHotels/Models/TableColumnAndTypes.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+public class TableColumnsAndTypes
+{
+    public Dictionary<string, string> Hotel { get; set; } = new();
+    public Dictionary<string, string> HotelChain { get; set; } = new();
+    public Dictionary<string, string> Address { get; set; } = new();
+    public Dictionary<string, string> HotelEmail { get; set; } = new();
+    public Dictionary<string, string> HotelPhone { get; set; } = new();
+    public Dictionary<string, string> HotelChainEmail { get; set; } = new();
+    public Dictionary<string, string> HotelChainPhone { get; set; } = new();
+    public Dictionary<string, string> HotelImage { get; set; } = new();
+    public Dictionary<string, string> HotelAmenity { get; set; } = new();
+    public Dictionary<string, string> Account { get; set; } = new();
+    public Dictionary<string, string> Employee { get; set; } = new();
+    public Dictionary<string, string> Room { get; set; } = new();
+    public Dictionary<string, string> RoomProblem { get; set; } = new();
+    public Dictionary<string, string> RoomAmenity { get; set; } = new();
+    public Dictionary<string, string> Booking { get; set; } = new();
+    public Dictionary<string, string> Customer { get; set; } = new();
+    public Dictionary<string, string> Renting { get; set; } = new();
+    public Dictionary<string, string> Review { get; set; } = new();
+}
